834c0ecaa43303f55706d944a3c1302636f71fa1
markcfleming <markcfleming1990@gmail.com>
2023-05-16 14:40:33 -0400

    working deploy errors

--- src/AlertPage.tsx
+++ src/AlertPage.tsx
@@ -1,47 +1,47 @@
-import { FormWrapper } from "./formWrapper";
-// import emailjs from "@emailjs/browser";
-// import{ useState } from "react"
-// import SignatureCanvas from "react-signature-canvas";
+// import { FormWrapper } from "./formWrapper";
+// // import emailjs from "@emailjs/browser";
+// // import{ useState } from "react"
+// // import SignatureCanvas from "react-signature-canvas";
 
-type CarrierData = {
-  carrierName: string;
-  carrierAddress: string;
-  mcnumber: string;
-  dotnumber: string;
-  // phoneNumber: string
-  // emailAddress: string
-  // img: {}
-};
+// type CarrierData = {
+//   carrierName: string;
+//   carrierAddress: string;
+//   mcnumber: string;
+//   dotnumber: string;
+//   // phoneNumber: string
+//   // emailAddress: string
+//   // img: {}
+// };
 
-type CarrierTermsProps = CarrierData & {
-  updateFields: (fields: Partial<CarrierData>) => void;
-};
+// type CarrierTermsProps = CarrierData & {
+//   updateFields: (fields: Partial<CarrierData>) => void;
+// };
 
-export function CarrierTerms({
-  carrierName,
-  carrierAddress,
-  mcnumber,
-  dotnumber,
-  // phoneNumber,
-  // emailAddress,
-  // img,
-  updateFields,
-}: CarrierTermsProps) {
-  return (
-    <FormWrapper title="Carrier Agreement">
-      <section>
-        <p>
-          This Agreement is entered into on "Date" "Todays Date" between MEGA
-          Logistics, LLC "Broker" <br />a registered Broker, MC-953505 and{" "}
-          {carrierName} located at {carrierAddress} a Registered Motor Carrier{" "}
-          <br />
-          {mcnumber}, {dotnumber} "Carrier".
-        </p>
+// export function CarrierTerms({
+//   carrierName,
+//   carrierAddress,
+//   mcnumber,
+//   dotnumber,
+//   // phoneNumber,
+//   // emailAddress,
+//   // img,
+//   updateFields,
+// }: CarrierTermsProps) {
+//   return (
+//     <FormWrapper title="Carrier Agreement">
+//       <section>
+//         <p>
+//           This Agreement is entered into on "Date" "Todays Date" between MEGA
+//           Logistics, LLC "Broker" <br />a registered Broker, MC-953505 and{" "}
+//           {carrierName} located at {carrierAddress} a Registered Motor Carrier{" "}
+//           <br />
+//           {mcnumber}, {dotnumber} "Carrier".
+//         </p>
         
         
-      </section>
-      <lable></lable>
-        <input></input>
-    </FormWrapper>
-  );
-}
+//       </section>
+//       <lable></lable>
+//         <input></input>
+//     </FormWrapper>
+//   );
+// }
